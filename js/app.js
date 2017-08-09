8a3f9f8dcc04e84a8e7f56396ba701189fa2f918
Yoav Benzion <yoavbenzion@gmail.com>
2017-08-09 16:56:58 +0300

    no message

--- js/app.js
+++ js/app.js
@@ -83,7 +83,7 @@ const WhatsAppBtn = document.querySelector('.WhatsAppBtn');
 
 WhatsAppBtn.addEventListener('click', ()=>{
   WhatsAppBtn.style.color = 'red';
-  location.href = '"whatsapp://send?text=Hello World!&phone=+972584909020"';
+  location.href = "whatsapp://send?text=Hello World!&phone=+972584909020";
 });
 
 
